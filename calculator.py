942c6a1bdb552fcabd5e437ed9b658b128656b50
hammam <h.besaiso95@gmail.com>
2020-02-05 13:34:01 +0200

    adding my calculater

--- calculator.py
+++ calculator.py
@@ -2,7 +2,34 @@
 def main():
 	#write your code here
 	pass
-	
+	n1=raw_input("Enter the first number: ")
+	n2=raw_input("Enter the second number: ")
+	op=raw_input("Choose the operation (+, -, /, *): ")
+	if n1.isdigit():
+		if n2.isdigit():
+			if op == '+':
+				an = int(n1) + int(n2)
+				print "The answer is " + str(an)
+			elif op == '-':
+				an = int(n1) - int(n2)
+				print "The answer is " + str(an)
+			elif op == '*':
+				an = int(n1) * int(n2)
+				print "The answer is " + str(an)
+			elif op == '/':
+				an = int(n1) / int(n2)
+				print "The answer is " + str(an)
+			else :
+				print " invalid operation "
+
+
+		else :
+			print " invalid second number"
+	else :
+		print " invalid first number"
+
+
+
 
 
 
